cd1d2ea5de650768147c73d4f8221c723f3f2279
Daniel Cano <daniel.cano.espinosa89@gmail.com>
2025-11-14 15:20:34 +0100

    dani

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!");
+        
+        
+        //exercici ordenació
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        
+        for (int i = 0; i < 5; i++) {
+        	numeros[i] = scanner.nextInt();
+        }
+        
+        //ordenació per selecció
+        for (int i = 0; i < numeros.length -1; i++) {
+        	int posicio = i;
+        	
+        	for (int j = 0+i; j < numeros.length; j++ ) {
+        		if (numeros[j] < numeros[posicio]) {
+        			posicio = j;
+        		}
+        	}
+        	
+        	int temp = numeros[posicio];
+        	numeros[posicio] = numeros[i];
+        	numeros[i] = temp;
+        }
+        
+        for (int i = 0; i < numeros.length; i++)
+        	System.out.println(numeros[i]);
     }
 }
